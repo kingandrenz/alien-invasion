d29ce122a99a64d30af5a4675a5f10141503579e
flextec <andrewandrenzanthony@gmail.com>
2023-11-25 15:33:07 +0100

    class functionality

---
+++ alien_invasion.py
@@ -0,0 +1,39 @@
+import sys
+import pygame
+from settings import Settings
+
+class AlienInvasion:
+    """Overall class to manage game asset and behaviour."""
+
+    def __init__(self):
+        """Initialize the game, and create game resource."""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_hight))
+        pygame.display.set_caption("Alien Invasion")
+
+        #set the background color
+        self.bg_color = (230, 230, 230)
+
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            #watch for keyboard and mouse event
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #redraw the screen through each pass through the loop
+            self.screen.fill(self.settings.bg_color)
+
+
+        #make the most recently drawn screen visible.
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    #make a game instance, and run the game.
+    ai= AlienInvasion()
+    ai.run_game()
