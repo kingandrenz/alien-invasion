216e3cc2a16b088c8950382b625ff73ac753ef64
flextec <andrewandrenzanthony@gmail.com>
2023-11-26 15:40:30 +0100

    piloting the ship: responding to key press, left or right

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,17 +23,32 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # Watch for keyboard and mouse events
+            self._check_events()
+            self._update_screen()
+
+
+    def _check_events(self):
+        # Respond  to keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        #move the ship to the right
+                        self.ship.rect.x += 1
+                    elif event.key == pygame.K_LEFT:
+                        #Move the ship to the left.
+                        self.ship.rect.x += 1
 
-            # Redraw the screen through each pass through the loop
+
+    def _update_screen(self):
+         #Update image on the screen, and flip to the new screen
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
+
 
 if __name__ == "__main__":
     # Make a game instance, and run the game.
