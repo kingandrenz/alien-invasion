e897ed235e0cd80a8f28bc2361790a4c5439ba98
flextec <andrewandrenzanthony@gmail.com>
2023-11-29 20:40:59 +0100

    Firing Bullets

--- bullet.py
+++ bullet.py
@@ -10,23 +10,22 @@ class Bullet(Sprite):
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
 
-        #craete a bullet at (0, 0) and the set correct position.
+        # Create a bullet at (0, 0) and then set the correct position.
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
-                self.settings.bullet_height)
+                                self.settings.bullet_height)
         self.rect.midtop = ai_game.ship.rect.midtop
 
-        # store the bullet's position as decimal
+        # Store the bullet's position as a decimal
         self.y = float(self.rect.y)
 
+    def update(self):
+        """Move the bullet up the screen"""
+        # Update the decimal position of the bullet.
+        self.y -= self.settings.bullet_speed
+        # Update the rect position
+        self.rect.y = self.y
 
-        def update(self):
-            """moving the bullet up the screen """
-            #update the decimal position of the bullet.
+    def draw_bullet(self):
+        """Draw the bullet to the screen"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
 
-            self.y -= self.settings.bullet_speed
-            # Update the rect position
-            self.rect.y = self.y
-
-        def draw_bullet(self):
-            """ draw the bullet to the screen"""
-            pygame.draw.rect(self.screen, self.color, self.rect)
--- settings.py
+++ settings.py
@@ -14,5 +14,6 @@ class Settings:
         #bullet settings
         self.bullet_speed = 1.0
         self.bullet_width = 3
+        self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
 
